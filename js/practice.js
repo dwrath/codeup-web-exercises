c921cd01cc54121d8615ddfc6b7c13d4defbecc6
Damian Wrather <damian.wrather@gmail.com>
2023-02-14 09:05:57 -0600

    conditionals exercise and practice

---
+++ js/practice.js
@@ -0,0 +1,39 @@
+//Write a function that accepts a string and returns the string reversed.
+
+// const reverseString = (str) => {
+//     let reverse = str.split("").reverse().join("");
+//
+//     return reverse;
+// }
+// console.log(reverseString('string'))
+
+//Write a function that accepts a last name and a first name and returns the full name.
+
+// const fullName = (lastName, firstName) =>{
+//     let name = `${firstName} ${lastName}`;
+//     return name
+// }
+// console.log(fullName('wrather', 'damian'))
+
+// Write a function called `identity(input)` that takes in an argument called
+// input and returns that input.
+
+// const identity = (input) =>{
+//     return input
+// }
+
+// Write a function called `getRandomNumber(min, max)` that returns a random
+// number between min and max values sent to that function call.
+
+// const getRandomNumber = (min, max) => {
+//     return Math.random() * (max - min) + min;
+// }
+
+// Write a function called `first(input)` that returns the first character in the
+// provided string.
+
+// const first = (input) => {
+//     let firstChar = input.charAt(0)
+//
+//     return firstChar;
+// }
