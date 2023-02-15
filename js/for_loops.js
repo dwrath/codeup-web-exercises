0457dfa0b735318e5f9d60013eb456f257708532
Damian Wrather <damian.wrather@gmail.com>
2023-02-15 14:29:57 -0600

    For loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -4,3 +4,24 @@ const showMultiplicationTable = (number) => {
         console.log(number * i);
     }
 }
+const randNum = ()=>{
+    for(let i = 0; i < 10; i++){
+        let randomNum = (Math.random() * (200 - 20) + 20).toFixed();
+        randomNum % 2 === 0 ? console.log(`${randomNum} is even`) : console.log(`${randomNum} is odd`)
+    }
+}
+const ramp = () => {
+    for (let i = 1; i < 10; i++) {
+        let row = i
+        for (let j = 1; j <= row; j++) {
+            console.log(`${i}`)
+        }
+    }
+}
+
+const subtract = (num) => {
+    while(num !== 0){
+        num -= 5
+        console.log(num)
+    }
+}
