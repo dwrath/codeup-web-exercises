b205ac8ef6c04e476416bb380018a31ff14843b6
Damian Wrather <damian.wrather@gmail.com>
2023-02-16 10:24:42 -0600

    For loops,break and continue, and while loops edit

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,8 +3,8 @@ const loop = () => {
     let num = 0
     do{
         num = parseFloat(prompt('Number to skip:'))
-
-    } while(typeof num !== 'number');
+        break
+    } while(typeof num !== 'number' || num > 50 && num > 0);
 
     for(let i = 1; i < 50; i+=2){
 
--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,7 @@ function formatToLocaleString(number, language='en', country='USD', currency='US
 function isNumericAndNotNaN(input){
     return !isNaN(parseFloat(input)) && input !== null && typeof input !== "boolean" && typeof input !== 'string';
 }
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min +1)+min);
+}
