2dec71e47749f80cb679355676ab851daf29818e
Damian Wrather <damian.wrather@gmail.com>
2023-02-17 11:08:23 -0600

    planets-array, planets-js

---
+++ js/planets-array.js
@@ -0,0 +1,48 @@
+(function(){
+    "use strict";
+
+    var planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
+
+    console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift("The Sun")
+    console.log(planets);
+
+    console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push('Pluto')
+    console.log(planets);
+
+    console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift()
+    console.log(planets);
+
+    console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop()
+    console.log(planets);
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log("Reversing the order of the planets array.");
+    let earth = planets.indexOf('Earth')
+    planets.reverse()
+    console.log(earth,planets);
+
+    console.log("Sorting the planets array.");
+    planets.sort((a,b)=>{
+        return a-b;
+    })
+    console.log(planets);
+})();
