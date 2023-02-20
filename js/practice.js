1e06a7142c81533ea010d85dd664f427a272a007
Damian Wrather <damian.wrather@gmail.com>
2023-02-20 11:41:37 -0600

    practice

--- js/practice.js
+++ js/practice.js
@@ -115,32 +115,3 @@ const getToDestination = (age, isInsured, hasCar) => {
     console.log(result);
 
 }
-// ### SWITCH STATEMENTS
-//
-// Let's make an application which helps a user input *validated* specs on a vehicle they are trying to sell:
-// - This will involve multiple switch statements and functions
-// - Orchestrate the statements in a central function that is responsible for calling and checking for errors
-//     each switch statement should be wrapped in a function which accepts the user's input as a string parameter and return that same string if it is validated
-//     ie: `function getBodyStyle(input){return input}`
-//
-//
-//     - For user input, either use the browser's prompt/confirm/alert methods or mock up user input by hard-coding values
-//
-//
-//     - The user should be able to enter their
-//     - Body style (2D, 4D, etc)
-//     - Vehicle type (sports car, sedan, compact, truck, etc)
-//     - Vehicle color
-//
-//
-//     - Each switch statement should confirm the user's choice in a logging statement
-//
-//
-//     - The point of this is to validate what the user enters.
-//     - If they do not enter an acceptable input, the DEFAULT statement will handle this as an error
-//         ie: default: `"error: " + vehicleType + " not recognized"`;
-//         - If this occurs, call the related function again (see: recursion) until the user enters a correct choice
-//         - HINT: a way you can check for errors is to see if the string contains the word "error"
-//
-//
-//         - After all switch statement functions successfully return the user's input, concatenate them all into a nicely-formatted string and return all data to the user
