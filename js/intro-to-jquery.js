71252610f29400df2c7112bf8a2a4b01dbd3d6ac
Damian Wrather <damian.wrather@gmail.com>
2023-03-17 10:48:28 -0500

    jquery intro

---
+++ js/intro-to-jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+    // $(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+// let listItem = $('#item-2').html();
+// alert(listItem);
+
+    //
+    // $('li').css('font-size', '20px');
+    // $('li').css('background-color', 'yellow');
+    // $('h1').css('background-color', 'yellow');
+    // $('p').css('background-color', 'yellow');
+    //
+    // let h1 = $('h1').html()
+    // alert(h1)
+    $('li, h1, p').css('background-color', 'yellow');
+})
+
