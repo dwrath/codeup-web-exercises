acb7747e6f84f548300ebf95130d046aff4ce57e
Damian Wrather <damian.wrather@gmail.com>
2023-03-20 16:51:13 -0500

    jquery_faq_2

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -10,4 +10,22 @@ $(document).ready(function() {
 
         $(this).toggleClass('active');
     });
+    $('.last-li').on('click', function (){
+        $('.park-facts').each(function (index){
+            $(this).children().last().css('background-color', 'yellow')
+        })
+    })
+    $('h3').on('click', function (){
+        $(this).next().children().css('font-weight', 'bold');
+    })
+    $('h3').hover(function (){
+        $(this).css('cursor', 'pointer');
+    })
+    $('li').hover(function (){
+        $(this).css('cursor', 'pointer');
+    })
+    $('li').on('click', function (){
+        $(this).parent().children().first().css('background-color', 'blue')
+    })
+
 })
