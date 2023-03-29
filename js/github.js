c3fc037bc658efd654351ef8e715175184700e41
Damian Wrather <damian.wrather@gmail.com>
2023-03-29 16:58:57 -0500

    promises

--- js/github.js
+++ js/github.js
@@ -1,13 +1,25 @@
 import keys from "./keys.js";
 export const getUserLastCommit = async (username)=>{
     try{
-        let response = await fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `${keys.github_token}`}})
+        let response = await fetch(`https://api.github.com/users/${username}/events/public`,
+            {headers: {'Authorization': `${keys.github_token}`}})
         let data = await response.json()
-        let date = data[0].created_at
-        return date
+        // let date = data[0].created_at
+        return data[0]
     }catch(error){
         console.log(error)
     }
+}
+export const getCommitDate = async (owner, repo)=>{
+    try{
+        let response = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits`,
+            {headers: {'Authorization': `${keys.github_token}`}})
+        let data = await response.json
+        return data
+    }catch(error){
+        console.log(error)
+    }
+
 }
 export const wait = (num)=>{
     return new Promise((resolve, reject) => {
@@ -15,3 +27,19 @@ export const wait = (num)=>{
     });
 }
 
+export const renderGithubUser = (user, parent) => {
+    const element = document.createElement('div');
+    element.classList.add('user-card');
+    element.innerHTML = `
+        <div class="img-wrapper">
+            <img src="${user.actor.avatar_url}" alt="User image" class="avatar">
+        </div>
+        <h2>${user.actor.display_login}</h2>
+        <a href="${user.actor.url}" target="_blank">Go to Profile</a>
+        <button>Remove</button>
+    `;
+    element.querySelector('button').addEventListener('click', function(){
+        element.remove();
+    });
+    parent.appendChild(element);
+}
--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,15 @@
-import{getUserLastCommit, wait} from "./github.js";
+import{getUserLastCommit, wait, renderGithubUser, getCommitDate} from "./github.js";
 
 (async ()=>{
-    console.log(await getUserLastCommit('dwrath'))
-    wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-    wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+    const user = await getUserLastCommit('dwrath')
+    const usersGrid = document.querySelector('#userGrid');
+    renderGithubUser(user, usersGrid)
+    const commit = await getCommitDate('dwrath', 'codeup-web-exercises')
+    // users.forEach(function(user){
+    //     renderGithubUser(user, usersGrid);
+    // });
+    // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+    // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 })()
 
