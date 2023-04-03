a63a6eb4a034fb9c7e51a90af54ea326c961cfc1
Damian Wrather <damian.wrather@gmail.com>
2023-04-03 08:44:14 -0500

    rotation on 3d carousel

--- js/movies.js
+++ js/movies.js
@@ -1,20 +1,12 @@
 import Movie from './components/Movie.js'
 
-//const carousel = document.querySelectorAll('.c1,.c2,.c3,.c4,.c5,.c6,.c7,.c8,.c9');
 const carousel = document.querySelector('.carousel');
-let cellCount = 9;
 const rightButton = document.querySelector('#left');
 const leftButton = document.querySelector('#right');
+const addMovie = document.querySelector('#add-movie')
 let selectedIndex = 0;
-let currentAngle = 0;
-const numSlides = carousel.length;
-const minAngle = 0;
-const maxAngle = (9 - 1) * 60;
+let cellCount = 9;
 
-function updateButtonStates() {
-    rightButton.disabled = (currentAngle >= maxAngle);
-    leftButton.disabled = (currentAngle <= minAngle);
-}
 function rotateCarousel() {
     let angle = selectedIndex / cellCount * -360;
     carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
@@ -29,7 +21,14 @@ leftButton.addEventListener('click', () => {
     rotateCarousel();
 
 });
-async function getMovies(){
+addMovie.addEventListener('click', ()=>{
+    document.querySelector('.overlay').classList.toggle('show');
+})
+document.querySelector('.cancel').addEventListener('click', function(event) {
+    document.querySelector('.overlay').classList.toggle('show');
+});
+
+    async function getMovies(){
     try{
         let response = await fetch( `https://codeup-json-server.glitch.me/movies`)
         let data = await response.json()
